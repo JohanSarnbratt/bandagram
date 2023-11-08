3862cf77aacb53e8ad6effdbe1429ed595da8527
Johan Särnbratt <johan@minna.tech>
2023-11-08 09:05:28 +0100

    pivot to bandagram

--- src/bandagram/bandagram.tsx
+++ src/bandagram/bandagram.tsx
@@ -1,24 +1,30 @@
 import React, {useMemo} from 'react';
+import {bands} from "./bands";
+import {shuffle} from "./shuffle";
 
 export const Bandagram = () => {
   const [text, setText] = React.useState('');
-  const answer = 'paramore';
+  const answer = useMemo(() => {
+    return bands[Math.floor(Math.random() * bands.length)];
+  }, []);
   const letters = useMemo(() => {
-    return answer
+    return shuffle(answer.toLowerCase()
       .split("")
-      .filter((char) => char !== " ")
-      .sort(() => Math.random() - 0.5);
-  }, [])
+      .filter((char) => char !== " "));
+  }, [answer])
+  const correct = text.toLowerCase() === answer.toLowerCase();
 
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
       <div>
         {letters.map(letter => {
-          return (<button>{letter}</button>);
+          const color = text.toLowerCase().includes(letter) ? "green" : undefined;
+          return (<button style={{backgroundColor: color}}>{letter}</button>);
         })}
       </div>
       <textarea style={{width: 300}} value={text} onChange={(event) => setText(event.target.value)}/>
-      <div>Result: {text === answer ? 'Correct!' : "That's not a band"}</div>
+      <div>Result: {correct ? 'Correct!' : "That's not a band"}</div>
+      <div style={{color: "white"}}>{answer}</div>
     </div>
   );
 }
