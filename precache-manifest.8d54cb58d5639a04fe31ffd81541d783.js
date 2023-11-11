b954e5c0f1eae66875e15942dcb5da0f7928d6e9
Johan Särnbratt <johan@minna.tech>
2023-11-11 16:43:17 +0100

    Updates

---
+++ precache-manifest.8d54cb58d5639a04fe31ffd81541d783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe67a7f9129d7f6769e2c1fcca15cf55",
+    "url": "/bandagram/index.html"
+  },
+  {
+    "revision": "ae4b1c62bb4dace97b60",
+    "url": "/bandagram/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "0780adb4419de5ee9a5f",
+    "url": "/bandagram/static/js/2.4b47fc34.chunk.js"
+  },
+  {
+    "revision": "8d1aa9f62f0e9bb87230",
+    "url": "/bandagram/static/js/3.a55009c7.chunk.js"
+  },
+  {
+    "revision": "ae4b1c62bb4dace97b60",
+    "url": "/bandagram/static/js/main.ac1894b2.chunk.js"
+  },
+  {
+    "revision": "fd0a31e1bc91d5474514",
+    "url": "/bandagram/static/js/runtime~main.7b8ac8f1.js"
+  }
+]);
